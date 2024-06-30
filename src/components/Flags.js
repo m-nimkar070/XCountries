490473908bad458e61b17f0c6f26abc8f1daa015
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-06-30 17:02:55 +0530

    completed

---
+++ src/components/Flags.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import "./flag.css";
+
+const Flags = ({url}) => {
+    const [flagData , setFlagData] = useState(null)
+    console.log(url)
+    const performApiCall =async (url)=>{
+        await fetch(url).then(
+            (res)=> res.json()
+        ).then(
+            (res)=>{
+                setFlagData(res)
+                console.log(res)
+            }
+        )
+    }
+
+    useEffect(
+        ()=>{
+            performApiCall(url);
+        }
+    ,[]);
+    
+  return (
+    <>
+        <div className='flag__Container'>
+            {flagData && flagData.map((item)=>
+                <div className="flag__card" key={item.name}>
+                <img src={item.flag} alt="a" />
+                <p>{item.name}</p>
+            </div>
+            )}
+        </div>
+    </>
+  )
+}
+
+export default Flags

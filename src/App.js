69ca9821d6a5e3cbb50773ca9e1f6368369b267a
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-07-09 13:37:37 +0530

    search funvtionality

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const fetchData= async()=>{
+    try {
+      const res = await fetch(URL);
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
-    fetch(URL)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok : ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => setCountries(data))
-      .catch((error) => console.error("Error fetching data:", error));
+    fetchData();
   }, []);
 
   const handleSearch = (e) => {
